cdf586c94289534400121172cfc6b0f264a39542
Lennoximus <lennoximus@gmail.com>
2017-10-25 13:40:32 +0300

    Add mixins and helpers for aspect-ratio and svg encoding

--- generators/lib/bundler/assets/styles-bundler/index.js
+++ generators/lib/bundler/assets/styles-bundler/index.js
@@ -12,15 +12,18 @@ const StylesBundler = () => {
       .then(() => {
         mkdirp('src/scss/styles/pages')
         mkdirp('src/scss/vendor')
+
         if (pagesLength !== 0) {
           pages.forEach(page => {
             generator.fs.write(`src/scss/styles/pages/_${page}.scss`, `.p-${page} {\n\n}`)
           })
         }
+
         generator.fs.write('src/scss/styles/_global.scss', '* {\r\n  box-sizing: border-box;\r\n}\r\n\r\nhtml {\r\n  width: 100%;\r\n\r\n}\r\n\r\nbody {\r\n  width: 100%;\r\n}\n\n.g {\n\n}')
         generator.fs.write('src/scss/styles/_freq.scss', '.f {\n\n}')
         generator.fs.copy(generator.templatePath('../utils/styles'), generator.destinationPath('src/scss/utils'))
-        generator.fs.write('src/scss/index.scss', '@import \'utils\/normalize\';\r\n@import \'utils\/variables\';\r\n@import \'utils\/mixins\';\r\n@import \'utils\/fonts\';\r\n\r\n@import \'styles\/global\';\r\n@import \'styles\/freq\';\r\n')
+        generator.fs.write('src/scss/index.scss', '@import \'utils\/normalize\';\r\n@import \'utils\/variables\';\r\n@import \'utils/helpers\';\n@import \'utils\/mixins\';\r\n@import \'utils\/fonts\';\r\n\r\n@import \'styles\/global\';\r\n@import \'styles\/freq\';\r\n')
+
         if (pagesLength !== 0) {
           pages.forEach(page => {
             generator.fs.append('src/scss/index.scss', `@import 'styles/pages/${page}';\r\n`)
@@ -39,15 +42,18 @@ const StylesBundler = () => {
         .then(() => {
           mkdirp('src/scss/styles/pages')
           mkdirp('src/scss/vendor')
+
           if (pagesLength !== 0) {
             pages.forEach(page => {
               generator.fs.write(`src/scss/styles/pages/_${page}.scss`, `.p-${page} {\n\n}`)
             })
           }
+
           generator.fs.write('src/scss/styles/_global.scss', '* {\r\n  box-sizing: border-box;\r\n}\r\n\r\nhtml {\r\n  width: 100%;\r\n\r\n}\r\n\r\nbody {\r\n  width: 100%;\r\n}\n\n.g {\n\n}')
           generator.fs.write('src/scss/styles/_freq.scss', '.f {\n\n}')
           generator.fs.copy(generator.templatePath('../utils/styles'), generator.destinationPath('src/scss/utils'))
           generator.fs.write('src/scss/index.scss', '@import \'utils\/normalize\';\r\n@import \'utils\/variables\';\r\n@import \'utils\/mixins\';\r\n@import \'utils\/fonts\';\r\n\r\n@import \'styles\/global\';\r\n@import \'styles\/freq\';')
+
           if (pagesLength !== 0) {
             pages.forEach(page => {
               generator.fs.append('src/scss/index.scss', `@import 'styles/pages/${page}';\r\n`)
